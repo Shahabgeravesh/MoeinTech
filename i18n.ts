5b6ddf469deda95e1caa68e138e7daa361a03c11
Shahab <Shahabgeravesh@gmail.com>
2026-01-23 11:07:06 -0800

    Fix next-intl config: Return locale in getRequestConfig

--- i18n.ts
+++ i18n.ts
@@ -5,9 +5,11 @@ export const locales = ['en', 'fa'] as const;
 export type Locale = (typeof locales)[number];
 
 export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as Locale)) notFound();
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
